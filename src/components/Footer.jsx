756a6b7625b4cd1cda18507f4e0d1027823f2858
Isaiah <Isaiahcwhite25@gmail.com>
2025-01-09 20:30:58 -0500

    Ecom-add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Logo from '../ecom-assets/Library.svg'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
